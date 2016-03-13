ee23df471d7c41f5adb3c12d43b1052ad8490539
LafeLabs <LafeLabs@gmail.com>
2016-03-13 00:31:53 -0700

    small arduino updates, more skeletron for Trash Stick core

--- ArduinoCode/list_of_Arduino_programs.md
+++ ArduinoCode/list_of_Arduino_programs.md
@@ -2,7 +2,6 @@
 
 * push a button, activate a pin, selected in the code or over serial
 * read from analog 4 or 5, selected in code or over serial
-* push a button, activate a pin, other button sets which pin is activated
 * read from a pin only when a button is pressed, button selects which pin to take data from
 * when you push a button it makes a coil go, and that coil is the next coil after the previous time a button was pressed or the one "before" it, depending on which button.  This is the one press one step 4 pole stepper motor, button pins can be replaced by external pins over the 6 pin port
 * simple feedback from one sense coil to one drive coil
---
+++ ArduinoCode/singleStepButtonDrive/singleStepButtonDrive.ino
@@ -0,0 +1,56 @@
+/*
+NO GODS NO MASTERS NO COPYRIGHT NO PATENT
+
+
+*/
+
+int pinArray[] = {5,6,9,10}; 
+int pinIndex = 0;
+int buttonPinA = 2;
+int buttonPinB = 3;
+boolean buttonStateA = LOW;
+boolean buttonStateB = LOW;
+boolean isOn = LOW;
+
+int x = 0;
+int y = 0;
+
+void setup() {
+  int x0 = analogRead(A4);
+  int y0 = analogRead(A5);  
+  pinMode(buttonPinA,INPUT);
+  pinMode(buttonPinB,INPUT);
+}
+
+void loop() {
+  buttonStateA = digitalRead(buttonPinA);//read first button[up]
+  buttonStateB = digitalRead(buttonPinB);//read second button[down]
+
+  if(buttonStateA && !buttonStateB && !isOn){//if up button pressed from off state
+    pinIndex += 1;  //increment
+    if(pinIndex > 3){ //if it's over 3, reset back to 0
+      pinIndex = 0;
+    }
+    isOn = HIGH; //after this cycles we need to be sure it's set to ON to avoid
+                //increment or decrement on next pass
+  }
+  if(buttonStateB && !buttonStateA && !isOn){//same for other button
+    pinIndex -= 1;
+    if(pinIndex < 0){
+      pinIndex = 3;
+    }
+    isOn = HIGH;
+  }
+
+  if(buttonStateA || buttonStateB){
+    isOn = HIGH;//set to high if any button is high, regardless of old or new
+  }  
+  if(!buttonStateA && !buttonStateB){
+    isOn = LOW;//if both buttons are off, set to OFF state 
+    analogWrite(pinArray[pinIndex],0);//and set pin to zero
+  }
+  else{ //"else" will only be when some button is high, in which case...
+    analogWrite(pinArray[pinIndex],255); //turn PIN high
+  }
+  delay(1); //wait a short time to let thigns settle
+}
